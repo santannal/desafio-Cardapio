c55a47c2d1516cbc9954b9d434c9648160683113
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-05-28 19:04:00 -0300

    closes #6 adicionando a lógica do site

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ var produtos = [
     { id: 2, name: "Coxa de Frango Crocante", price: 25.00 },
     { id: 3, name: "Carne de Panela", price: 22.00 },
     { id: 4, name: "Farofa", price: 10.00 },
-    { id: 5, name: "Salada", price: 8.00 },
+    { id: 5, name: "Mini Salada", price: 8.00 },
     { id: 6, name: "Torresmo", price: 12.00 }
 ];
 
 function calc() {
     var output = document.getElementById("output");
     var quantities = document.getElementsByName("quantity");
+    var client = document.getElementById("clienteNome").value;
     var total = 0;
     output.innerHTML = "";
 
@@ -20,4 +21,15 @@ function calc() {
         currency: 'BRL',
     });
 
+    output.innerHTML += `<b>Caro</b> ${client}</br></br></br>Seguem os dados do seu pedido.</br></br>O seu pedido é:</br></br>`;
+
+    for (var input of quantities) {
+        var id = input.id;
+        if (input.value > 0) {
+            output.innerHTML += `<li>Prato: ${produtos[id - 1].name} - Preço Unitário: ${formatter.format(produtos[id - 1].price)} - Quantidade: ${input.value} - Valor total: ${formatter.format(input.value * produtos[id - 1].price)}</br>`;
+            total += produtos[id - 1].price * input.value;
+        }
+    }
+
+    output.innerHTML += `</br></br><h4>Preço final: ${formatter.format(total)}`;
 }
