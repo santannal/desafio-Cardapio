2939710f58b3d63140ec5d358a3f80cc44b7cb3a
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-05-28 18:06:57 -0300

    closes #5 Index.html : Componente quantidade e soma moeda

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 $("#phone").mask("(99) 9999-99999");
 
+var produtos = [
+    { id: 1, name: "Bife com Batata", price: 30.00 },
+    { id: 2, name: "Coxa de Frango Crocante", price: 25.00 },
+    { id: 3, name: "Carne de Panela", price: 22.00 },
+    { id: 4, name: "Farofa", price: 10.00 },
+    { id: 5, name: "Salada", price: 8.00 },
+    { id: 6, name: "Torresmo", price: 12.00 }
+];
+
+function calc() {
+    var output = document.getElementById("output");
+    var quantities = document.getElementsByName("quantity");
+    var total = 0;
+    output.innerHTML = "";
+
+    var formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    });
+
+}
